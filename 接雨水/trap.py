2d1ac75e4753a62ce89ae192cc2cd0cb4d4358e4
deb91049 <1260908357@qq.com>
2020-11-28 21:24:05 +0800

    leetcode

---
+++ 接雨水/trap.py
@@ -0,0 +1,67 @@
+# 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+#
+#  
+#
+# 示例 1：
+#
+#
+#
+# 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+# 输出：6
+# 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+# 示例 2：
+#
+# 输入：height = [4,2,0,3,2,5]
+# 输出：9
+#  
+#
+# 提示：
+#
+# n == height.length
+# 0 <= n <= 3 * 104
+# 0 <= height[i] <= 105
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/trapping-rain-water
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+
+        if len(height) <= 1:
+            return 0
+
+        max_height = 0
+        max_height_index = 0
+
+        # 找到最高点
+        for i in range(len(height)):
+            h = height[i]
+            if h > max_height:
+                max_height = h
+                max_height_index = i
+
+        area = 0
+
+        # 从左边往最高点遍历
+        tmp = height[0]
+        for i in range(max_height_index):
+            if height[i] > tmp:
+                tmp = height[i]
+            else:
+                area = area + (tmp - height[i])
+
+        # 从右边往最高点遍历
+        tmp = height[-1]
+        for i in reversed(range(max_height_index + 1, len(height))):
+            if height[i] > tmp:
+                tmp = height[i]
+            else:
+                area = area + (tmp - height[i])
+
+        return area
+    print(trap("",[0,1,0,2,1,0,1,3,2,1,2,1]))
---
+++ 电话号码的字母组合/letterCombinations.py
@@ -0,0 +1,37 @@
+# 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+#
+# 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+#
+#
+#
+# 示例:
+#
+# 输入："23"
+# 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        KEY = {'2': ['a', 'b', 'c'],
+               '3': ['d', 'e', 'f'],
+               '4': ['g', 'h', 'i'],
+               '5': ['j', 'k', 'l'],
+               '6': ['m', 'n', 'o'],
+               '7': ['p', 'q', 'r', 's'],
+               '8': ['t', 'u', 'v'],
+               '9': ['w', 'x', 'y', 'z']}
+        if digits == '':
+            return []
+        ans = ['']
+        for num in digits:
+            ans = [pre+suf for pre in ans for suf in KEY[num]]
+        return ans
+
+    print(letterCombinations("","23"))
